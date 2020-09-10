6e9c5210fb254b6b329e3b476c434ccf09425153
L.desk <54053176+ocholuo@users.noreply.github.com>
2020-09-10 10:53:51 -0400

    9.10

--- _posts/0.project/autoscript/0.autoscript_storage_s3_or_locally/pgbackup/src/pgbackup/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-
-# To create a package within your Python project
-# need to add an __init__.py file inside of a directory with the desired package name.
-# All other Python files within that directory will be modules on the package.
--- _posts/0.project/autoscript/0.autoscript_storage_s3_or_locally/pgbackup/src/pgbackup/storage.py
+++
@@ -1,9 +0,0 @@
-
-
-def local(infile, outfile):
-    outfile.write(infile.read())
-    outfile.close()
-    infile.close()
-
-def s3(client, infile, bucket, name):
-    client.upload_fileobj(infile, bucket, name)
